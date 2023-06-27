cfbf678ea704eed6a4daad40e988039efae53c01
Shweta Pal <shwetapal975@gmail.com>
2023-06-27 21:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 document.getElementById('enterBtn').addEventListener('click', ()=>{
 	let inputElement = document.getElementById('status')
-	inputElement.innerHtML = '<h1 id="status">Entered Metaverse</h1>';
+	inputElement.innerHtML = '<h1>Entered Metaverse</h1>';
 		
 })
