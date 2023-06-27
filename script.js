dbafe996e56a292a253e37b5aaf3e4fdf7a51d8a
Shweta Pal <shwetapal975@gmail.com>
2023-06-27 21:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.getElementById('enterBtn').addEventListener('click', ()=>{
+	let inputElement = document.getElementById('status')
+	inputElement.outerHTML = '<h1 id="status">Entered the Metaverse</h1>';
+		
+})
